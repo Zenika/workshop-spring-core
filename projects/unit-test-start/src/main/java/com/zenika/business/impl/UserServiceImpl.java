7fa383c86d216885a1ca0b0625bfd23d910530fd
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2012-10-26 22:54:58 +0200

    TP tests unitaires

---
+++ projects/unit-test-start/src/main/java/com/zenika/business/impl/UserServiceImpl.java
@@ -0,0 +1,123 @@
+/**
+ * 
+ */
+package com.zenika.business.impl;
+
+import java.util.List;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.zenika.business.UserService;
+import com.zenika.domain.User;
+import com.zenika.repository.UserRepository;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public class UserServiceImpl implements UserService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
+	private UserRepository userRepository;
+	
+	private String digest;
+	
+	private Encoder encoder;
+	
+	/* (non-Javadoc)
+	 * @see com.zenika.business.UserService#authenticate(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public User authenticate(String login, String password) {
+		// TODO 01 analyser le code de la méthode d'authentification
+		// nous allons tester les différents chemins,
+		// en utilisant un mock pour le UserRepository
+		User user = userRepository.getByLogin(login);
+		if(user == null) {
+			return null;
+		} else {
+			if(password == null) {
+				throw new IllegalArgumentException("Le mot de passe ne peut être nul !");
+			}
+			password = encoder.encode(password);
+			if(password.equals(user.getPassword())) {
+				return user;
+			} else {
+				return null;
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see com.zenika.business.UserService#create(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public User create(String login, String password) {
+		User user = userRepository.getByLogin(login);
+		if(user == null) {
+			user = userRepository.create(login, encoder.encode(password));
+		} else {
+			throw new IllegalArgumentException("Un utilisateur avec ce login existe déjà");
+		}
+		return user;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.zenika.business.UserService#list()
+	 */
+	@Override
+	public List<User> list() {
+		return userRepository.list();
+	}
+	
+	public void init() {
+		if(digest == null || digest.trim().length() == 0) {
+			LOGGER.info("Pas de hachage pour les mots de passe");
+			encoder = new Encoder() {
+				@Override
+				public String encode(String input) {
+					return input;
+				}
+			};
+		} else {
+			LOGGER.info("Utilisation de l'algorithme {} pour le hachage des mots de passe",digest);
+			encoder = new MessageDigestEncoder(digest);
+		}
+	}
+	
+	private interface Encoder {
+		
+		String encode(String input);
+		
+	}
+	
+	private class MessageDigestEncoder implements Encoder {
+		
+		private final String algorithm;
+		
+		public MessageDigestEncoder(String algorithm) {
+			this.algorithm = algorithm;
+		}
+		
+		@Override
+		public String encode(String input) {
+			byte[] bytes = StringUtils.getBytesUtf8(input);
+			return Hex.encodeHexString(DigestUtils.getDigest(algorithm).digest(bytes));
+		}
+	}
+	
+	public void setUserRepository(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	public void setDigest(String digest) {
+		this.digest = digest;
+	}
+	
+}
---
+++ projects/unit-test-start/src/test/java/com/zenika/business/impl/UserServiceImplTest.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package com.zenika.business.impl;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.zenika.domain.User;
+import com.zenika.repository.UserRepository;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+// TODO 02 supprimer @Ignore
+@Ignore
+public class UserServiceImplTest {
+
+	private UserRepository userRepository;
+	
+	private UserServiceImpl userService;
+	
+	@Before public void setUp() {
+		// TODO 03 initialiser le mock
+		// le mock est ensuite injecté dans le service
+		
+		userService = new UserServiceImpl();
+		userService.setUserRepository(userRepository);
+		userService.init();
+	}
+	
+	@Test public void getByLoginNoUser() {
+		String login = "test";
+		// TODO 04 programmer le mock pour qu'il retourne null sur l'appel de getByLogin
+
+		// TODO 05 s'assurer que la méthode UserService.authenticate retourne null
+		
+		// on lance la vérification sur les appels au mock
+		verify(userRepository).getByLogin(login);
+	}
+	
+	@Test public void getByLoginNullEmptyPassword() {
+		// TODO 06 regarder comment tester une méthode qui lance une exception
+		String login = "test";
+		when(userRepository.getByLogin(login)).thenReturn(new User(1L, login, "whatever"));
+		try {
+			userService.authenticate(login, null);
+			Assert.fail("Utilisateur existant, mais mot de passe entré nul, une exception aurait du être lancée");
+		} catch(IllegalArgumentException e) {
+			// OK
+		}
+		verify(userRepository).getByLogin(login);
+	}
+	
+	@Test public void getByLoginPassordKo() {
+		String login = "test";
+		when(userRepository.getByLogin(login)).thenReturn(new User(1L, login, "secret"));
+		Assert.assertNull(userService.authenticate(login, "bad password"));
+		verify(userRepository).getByLogin(login);
+	}
+	
+	@Test public void getByLoginPassordOk() {
+		String login = "test";
+		when(userRepository.getByLogin(login)).thenReturn(new User(1L, login, "secret"));
+		Assert.assertNotNull(userService.authenticate(login, "secret"));
+		verify(userRepository).getByLogin(login);
+	}
+	
+}
