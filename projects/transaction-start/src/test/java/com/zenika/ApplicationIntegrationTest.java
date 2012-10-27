f3a9bec91af3458a8d3c25ef401f63353e8e7a22
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2012-10-27 16:11:48 +0200

    TP transaction

---
+++ projects/transaction-start/src/test/java/com/zenika/ApplicationIntegrationTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.zenika;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.zenika.business.UserService;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/test-application-configuration.xml")
+public class ApplicationIntegrationTest {
+
+	@Autowired private UserService userService;
+	
+	@Test public void createAndAuthenticateUser() {
+		// TODO 07 lancer le test
+		// on peut s'assurer que les transactions sont bien activées
+		// en rendant le repository transactionnel, avec un niveau de propagation
+		// à mandatory. Il suffit ensuite de commenter les annotations @Transactional
+		// du service et le test doit échouer.
+		String login = "mmouse";
+		String password = "this is a test";
+		userService.create(login,password);
+		Assert.assertNotNull(userService.authenticate(login, password));
+	}
+	
+}
