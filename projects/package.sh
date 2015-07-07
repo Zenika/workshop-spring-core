25adadafe8e7da5d98d53e9c9aaabc9a90ef60eb
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2015-07-07 17:49:41 +0200

    Améliorer le packaging

--- projects/package.sh
+++ projects/package.sh
@@ -1,8 +1,18 @@
-rm -rf ../repository
-mkdir ../repository
-mvn clean dependency:copy-dependencies -Dmdep.useRepositoryLayout -Dclassifier=sources -Dsilent
-mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout -Dsilent
+rm -rf ../build
+mkdir -p ../build/repository
+mkdir -p ../build/projects
+mvn clean dependency:copy-dependencies -Dmdep.useRepositoryLayout -Dclassifier=sources -q
+mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout -q
 for d in */ ; do
-    cp -r "$d"target/dependency/* ../repository
+    cp -r "$d"target/dependency/* ../build/repository
 done
-mvn clean eclipse:clean eclipse:eclipse -DdownloadSources=true
+mvn clean eclipse:clean eclipse:eclipse -q
+cp -r . ../build/projects
+mvn clean eclipse:clean -q
+find ../build/projects -type f -name '*.iml' -delete
+rm -rf ../build/projects/.git
+rm -rf ../build/projects/package.sh
+rm -rf ../build/projects/.idea
+rm -rf ../build/projects/.settings
+rm -rf ../build/projects/*/.settings
+cd ../build/ && zip -r spring-workshop.zip .
