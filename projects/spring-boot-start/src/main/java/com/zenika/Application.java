c5248160fcfcebaa358015e6fde2d0cbb88abfa7
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2015-08-21 14:36:03 +0200

    Créer les slides Spring Boot

--- projects/spring-boot-start/src/main/java/com/zenika/Application.java
+++ projects/spring-boot-start/src/main/java/com/zenika/Application.java
@@ -2,7 +2,6 @@ package com.zenika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
